a5d9b57b30f0b106e03ab1c47bdd5eea93bfdb55
iamargelh <fernandohu93@gmail.com>
2023-04-23 17:36:21 -0700

    Update generate-index.py

--- generate-index.py
+++ generate-index.py
@@ -29,81 +29,83 @@ def generate_back_element(path):
 
 def generate_html(tree):
     html = '''<!DOCTYPE html>
-<html>
-<head>
-  <title>Index</title>
-  <script>
-    const tree = ''' + str(tree) + ''';
-    
-    function generateLink(path) {
-      return `https://${owner}.github.io/${repo}/${path}`;
-    }
-    
-    function updatePath(path) {
-      const parts = path.split('/');
-      let currentPath = '';
-      pathSpan.innerHTML = '';
-      for (let i = 0; i < parts.length; i++) {
-        if (i > 0) {
-          currentPath += '/';
-          pathSpan.appendChild(document.createTextNode('/'));
+    <html>
+    <head>
+      <title>Index</title>
+      <script>
+        const tree = ''' + str(tree) + ''';
+        
+        function generateLink(path) {
+          return `https://${owner}.github.io/${repo}/${path}`;
         }
-        currentPath += parts[i];
-        const partSpan = document.createElement('span');
-        partSpan.innerHTML = parts[i];
-        partSpan.style.color = 'blue';
-        partSpan.style.cursor = 'pointer';
-        partSpan.addEventListener('click', () => {
-          updatePath(currentPath);
-          updateContents(currentPath);
-        });
-        pathSpan.appendChild(partSpan);
-      }
-    }
-    
-    function updateContents(path) {
-      contentsSpan.innerHTML = '';
-      if (path !== rootFolder) {
-        contentsSpan.innerHTML += `''' + generate_back_element(root_folder) + '''`;
-      }
-      
-      for (const item of tree) {
-        if (item.path.startsWith(path + '/') && !item.path.slice(path.length + 1).includes('/')) {
-          if (item.type === 'tree') {
-            contentsSpan.innerHTML += `''' + generate_folder_element('${item["path"].split("/")[-1]}', item['path']) + '''`;
-          } else if (item.type === 'blob') {
-            contentsSpan.innerHTML += `''' + generate_file_element('${item["path"].split("/")[-1]}', item['path']) + '''`;
+        
+        function updatePath(path) {
+          const parts = path.split('/');
+          let currentPath = '';
+          pathSpan.innerHTML = '';
+          for (let i = 0; i < parts.length; i++) {
+            if (i > 0) {
+              currentPath += '/';
+              pathSpan.appendChild(document.createTextNode('/'));
+            }
+            currentPath += parts[i];
+            const partSpan = document.createElement('span');
+            partSpan.innerHTML = parts[i];
+            partSpan.style.color = 'blue';
+            partSpan.style.cursor = 'pointer';
+            partSpan.addEventListener('click', () => {
+              updatePath(currentPath);
+              updateContents(currentPath);
+            });
+            pathSpan.appendChild(partSpan);
           }
         }
-      }
-    }
-    
-    let tree;
-    
-    function init() {
-      tree = ''' + str(tree) + ''';
-      
-      updatePath(rootFolder);
-      
-      updateContents(rootFolder);
-    }
-    
-    init();
-  </script>
-</head>
-<body>
-  <span id="path"></span><br><br>
-  <span id="contents"></span>
-  <script>
-    const owner = \'''' + owner + '''\';
-    const repo = \'''' + repo + '''\';
-    const rootFolder = \'''' + root_folder + '''\';
-    
-    const pathSpan = document.querySelector('#path');
-    const contentsSpan = document.querySelector('#contents');
-  </script>
-</body>
-</html>'''
+        
+        function updateContents(path) {
+          contentsSpan.innerHTML = '';
+          if (path !== rootFolder) {
+            contentsSpan.innerHTML += `''' + generate_back_element(root_folder) + '''`;
+          }
+          
+          for (let i = 0; i < tree.length; i++) {
+            let item = tree[i];
+            if (item.path.startsWith(path + '/') && !item.path.slice(path.length + 1).includes('/')) {
+              if (item.type === 'tree') {
+                contentsSpan.innerHTML += `''' + generate_folder_element('${item["path"].split("/")[-1]}', item['path']) + '''`;
+              } else if (item.type === 'blob') {
+                contentsSpan.innerHTML += `''' + generate_file_element('${item["path"].split("/")[-1]}', item['path']) + '''`;
+              }
+            }
+          }
+        }
+        
+        let tree;
+        
+        function init() {
+          tree = ''' + str(tree) + ''';
+          
+          updatePath(rootFolder);
+          
+          updateContents(rootFolder);
+        }
+        
+        init();
+      </script>
+    </head>
+    <body>
+      <span id="path"></span><br><br>
+      <span id="contents"></span>
+      <script>
+        const owner = \'''' + owner + '''\';
+        const repo = \'''' + repo + '''\';
+        const rootFolder = \'''' + root_folder + '''\';
+        
+        const pathSpan = document.querySelector('#path');
+        const contentsSpan = document.querySelector('#contents');
+      </script>
+    </body>
+    </html>
+    '''
     
     with open('index.html', 'w') as file:
         file.write(html)
