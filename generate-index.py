b6b7f4ed47fc1ecf4128f31465755f4ae791837d
iamargelh <fernandohu93@gmail.com>
2023-04-23 18:44:20 -0700

    Update generate-index.py

--- generate-index.py
+++ generate-index.py
@@ -1,68 +1,120 @@
+import requests
+import base64
 import os
-from jinja2 import Template
 
-# Obtener la ruta de la carpeta raíz del repositorio
-repo_path = os.getcwd()
+# Configuración
+owner = 'iamargelh'
+repo = 'ArgelH-Subs'
+root_folder = 'subs'
+api_url = f'https://api.github.com/repos/{owner}/{repo}/contents/{root_folder}'
 
-# Obtener la ruta de la carpeta subs y construir el link a la página de GitHub Pages
-subs_path = os.path.join(repo_path, 'subs')
-github_pages_link = 'https://{username}.github.io/{repository_name}/subs/'.format(
-    username='iamargelh,
-    repository_name='ArgelH-Subs')
+# Función para obtener el contenido de un archivo
+def get_file_content(path):
+    response = requests.get(f'https://raw.githubusercontent.com/{owner}/{repo}/main/{root_folder}/{path}')
+    return response.content.decode()
 
-# Recorrer la estructura de carpetas y archivos y construir una lista de diccionarios con la información necesaria
-file_tree = []
+# Función para obtener el árbol de archivos y carpetas
+def get_tree(path=''):
+    response = requests.get(api_url + ('' if not path else f'/{path}'))
+    data = response.json()
+    tree = {}
+    for item in data:
+        if item['type'] == 'dir':
+            tree[item['name']] = get_tree(item['path'].replace(f'{root_folder}/', ''))
+        else:
+            content = base64.b64decode(item['content']).decode()
+            tree[item['name']] = content
+    return tree
 
-for root, dirs, files in os.walk(subs_path):
-    # Construir el link para la carpeta actual
-    path_parts = os.path.relpath(root, start=subs_path).split(os.sep)
-    current_path = os.path.join(github_pages_link, *path_parts)
-    current_name = path_parts[-1] if path_parts else 'subs'
-
-    # Añadir la carpeta actual a la lista de archivos y carpetas
-    file_tree.append({
-        'name': current_name,
-        'link': current_path,
-        'type': 'folder'
-    })
-
-    # Recorrer los archivos de la carpeta actual y añadirlos a la lista de archivos y carpetas
-    for file_name in files:
-        file_path = os.path.join(root, file_name)
-        file_link = os.path.join(current_path, file_name[:-3])
-        file_tree.append({
-            'name': file_name[:-3],
-            'link': file_link,
-            'type': 'file'
-        })
-
-# Ordenar la lista de archivos y carpetas alfabéticamente
-file_tree.sort(key=lambda x: x['name'].lower())
-
-# Construir el HTML utilizando jinja2 y escribirlo en un archivo index.html
-template = Template('''
+# Generar el archivo index.html con el contenido del árbol de archivos y carpetas
+html_template = '''
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Explorador de archivos y carpetas</title>
-</head>
-<body>
-    <div id="explorer">
-        <div id="path">{{ path }}</div>
-        <div id="contents">
-            {% for item in file_tree %}
-                {% if item['type'] == 'folder' %}
-                    <span>↪ 📁 <a href="{{ item['link'] }}">{{ item['name'] }}</a></span>
-                {% else %}
-                    <span>↪ 📄 <a href="{{ item['link'] }}">{{ item['name'] }}</a></span>
-                {% endif %}
-            {% endfor %}
-        </div>
-    </div>
-</body>
-</html>
-''')
 
+<head>
+    <title>(ArgelH-Subs)</title>
+    <link rel="icon" href="https://i.imgur.com/jycGdTy.png" />
+    <style>
+        html {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 62.5%;
+        }
+
+        body {
+            font-size: 3.2rem;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="path"></div>
+    <div id="contents"></div>
+
+    <script>
+        let tree = {{tree}};
+        let path = '';
+
+        function showTree() {
+            let contentsDiv = document.getElementById('contents');
+            contentsDiv.innerHTML = '';
+            let pathSpan = document.getElementById('path');
+            pathSpan.innerHTML = '';
+            let parts = path.split('/');
+            for (let i in parts) {
+                let part = parts[i];
+                if (part) {
+                    let partSpan = document.createElement('span');
+                    partSpan.innerText = part;
+                    partSpan.onclick = () => {
+                        path = parts.slice(0, i).join('/');
+                        showTree();
+                    };
+                    pathSpan.appendChild(partSpan);
+                    if (i < parts.length - 1) {
+                        pathSpan.innerHTML += '/';
+                    }
+                }
+            }
+            if (path) {
+                let backSpan = document.createElement('div');
+                backSpan.innerHTML = '⤴ 📂 ..';
+                backSpan.onclick = () => {
+                    path = parts.slice(0, -1).join('/');
+                    showTree();
+                };
+                contentsDiv.appendChild(backSpan);
+            }
+            for (let name in tree) {
+                let item = tree[name];
+                if (typeof item === 'object') {
+                    if (item.type === 'dir') {
+                        let folderSpan = document.createElement('div');
+                        folderSpan.innerHTML = `↪ 📁 ${name}`;
+                        folderSpan.onclick = () => {
+                            path = `${path}/${name}`;
+                            showTree();
+                        };
+                        contentsDiv.appendChild(folderSpan);
+                    } else if (item.type === 'file') {
+                        let fileSpan = document.createElement('div');
+                        fileSpan.innerHTML = `↪ 📄 ${name.replace('.md', '')}`;
+                        fileSpan.onclick = () => {
+                            window.location.href =
+                                `https://${owner}.github.io/${repo}/${rootFolder}/${path}/${name}`.replace('.md', '');
+                        };
+                        contentsDiv.appendChild(fileSpan);
+                    }
+                }
+            }
+        }
+        showTree(tree);
+    </script>
+
+</body>
+
+</html>
+'''
+
+# Escribir el contenido del archivo index.html en el disco
 with open(os.path.join(repo_path, 'index.html'), 'w') as f:
     f.write(template.render(path='subs', file_tree=file_tree))
