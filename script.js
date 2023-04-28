9af968c8de93c95fb9911e5629bdc8baa91a866b
iamargelh <fernandohu93@gmail.com>
2023-04-28 11:51:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ fetch("tree.json")
                 if (naming[index] == naming[0]) {
                     pathElement.innerHTML = `ArgelH /<a href="#" class="folder">${naming[index]}</a>`;
                 } else {
-                    pathElement.innerHTML = `/<a href="#" class="folder">${naming[index]}</a>U+0020`;
+                    pathElement.innerHTML = `/<a href="#" class="folder">${naming[index]}</a> `;
                 }
                 pathDiv.appendChild(pathElement);
             }
